7f3e7d7196a684662358e8080eaf3c871c5ba67d
kazuki saito <kazuki.s0711@gmail.com>
2018-05-12 02:52:57 +0900

    bootstrap - login - Environment

---
+++ src/main/java/org/sko/controller/LoginController.java
@@ -0,0 +1,27 @@
+package org.sko.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.sko.form.LoginForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/login")
+    public String Login(Model model) {
+        
+        model.addAttribute("loginForm", new LoginForm());
+        
+        return "login/index";
+    }
+    
+    @RequestMapping("/login/do")
+    public String LoginDo(Model model, @Valid LoginForm loginForm, BindingResult bindingResult, HttpServletRequest request) {
+        
+        return "login/index";
+    }
+}
---
+++ src/main/java/org/sko/form/LoginForm.java
@@ -0,0 +1,28 @@
+package org.sko.form;
+
+import javax.validation.constraints.Size;
+
+public class LoginForm {
+    
+    @Size(min=1, max=50)
+    private String username;
+
+    @Size(min=1, max=50)
+    private String password;
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
